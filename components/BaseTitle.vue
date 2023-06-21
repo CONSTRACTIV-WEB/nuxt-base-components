e4e80d95aa096acce7aab4717f15ea0041de1a9f
Stefan Huffer <s.huffer@constractiv.de>
2023-06-21 12:00:18 +0200

    baseline component

--- components/BaseTitle.vue
+++ components/BaseTitle.vue
@@ -1,22 +1,5 @@
 <template>
-  <component :is="tag" :class="tc({ theme: props.theme })">
-    <slot />
-  </component>
+  <h1><slot /></h1>
 </template>
 <script setup>
-  import { tv } from 'tailwind-variants'
-
-  const props = defineProps({
-    tag: {
-      type: String,
-      default: 'h2',
-    },
-    theme: {
-      type: String,
-      required: true,
-    },
-  })
-
-  const { theme } = useAppConfig()
-  const tc = tv(theme.heading)
 </script>
