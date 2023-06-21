b01d9851f4eaa5a63db9048aeb0cb4c8410252e9
Stefan Huffer <s.huffer@constractiv.de>
2023-06-21 12:05:26 +0200

    update: modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,6 @@
   export default defineNuxtConfig({
     // Extending from base nuxt.config.ts!
-    app: {
-      head: {
-        title: 'Extending Configs is Fun!',
-        meta: [
-          { name: 'description', content: 'I am using the extends feature in nuxt 3!' }
-        ],
-      }
-    }
+    modules: [
+      'tailwind-variants'
+    ],
   })
