333c2144da6038825faeede1651e26efef0fab86
Stefan Huffer <s.huffer@constractiv.de>
2023-06-21 11:34:58 +0200

    add title base component

---
+++ components/BaseTitle.vue
@@ -0,0 +1,22 @@
+<template>
+  <component :is="tag" :class="tc({ theme: props.theme })">
+    <slot />
+  </component>
+</template>
+<script setup>
+  import { tv } from 'tailwind-variants'
+
+  const props = defineProps({
+    tag: {
+      type: String,
+      default: 'h2',
+    },
+    theme: {
+      type: String,
+      required: true,
+    },
+  })
+
+  const { theme } = useAppConfig()
+  const tc = tv(theme.heading)
+</script>
