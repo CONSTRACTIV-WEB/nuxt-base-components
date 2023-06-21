fe8e5ce4008c83c9b537b3212c426a075809f957
Stefan Huffer <95777086+thedayitallcame@users.noreply.github.com>
2023-06-21 21:25:28 +0200

    Update BaseTitle.vue

--- components/BaseTitle.vue
+++ components/BaseTitle.vue
@@ -1,5 +1,5 @@
 <template>
-  <component :is="tag">
+  <component :is="tag" :class="button({ color: primary })">
     <slot />
   </component>
 </template>
@@ -12,5 +12,16 @@
       default: 'h2',
     }
   })
+
+  const button = tv({
+  base: "font-semibold text-white text-sm py-1 px-4 rounded-full active:opacity-80",
+  variants: {
+    color: {
+      primary: "bg-blue-500 hover:bg-blue-700",
+      secondary: "bg-purple-500 hover:bg-purple-700",
+      success: "bg-green-500 hover:bg-green-700",
+    },
+  },
+});
 
 </script>
